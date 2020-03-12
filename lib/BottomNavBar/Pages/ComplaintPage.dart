5fba6bb122a65b9619fc90c5530045e322e4be63
yashraut362 <yashraut362@gmail.com>
2020-03-12 22:16:27 +0530

    stab project

---
+++ lib/BottomNavBar/Pages/ComplaintPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class ComplaintPage extends StatefulWidget {
+  @override
+  _ComplaintPageState createState() => _ComplaintPageState();
+}
+
+class _ComplaintPageState extends State<ComplaintPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
