cfd984431029e8f03a6e915fb212b0f2d861632e
yashraut362 <yashraut362@gmail.com>
2020-03-13 21:46:14 +0530

    events done

--- lib/BottomNavBar/Pages/ComplaintPage.dart
+++ lib/BottomNavBar/Pages/ComplaintPage.dart
@@ -1,5 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
+import 'package:expansion_tile_card/expansion_tile_card.dart';
 
 class ComplaintPage extends StatefulWidget {
   @override
@@ -8,7 +9,132 @@ class ComplaintPage extends StatefulWidget {
 
 class _ComplaintPageState extends State<ComplaintPage> {
   @override
+  void initState() {
+    super.initState();
+    getDriversList().then((results) {
+      setState(() {
+        querySnapshot = results;
+      });
+    });
+  }
+
+  QuerySnapshot querySnapshot;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: showDrivers(),
+    );
+  }
+
+  //build widget as prefered
+  //i'll be using a listview.builder
+  Widget showDrivers() {
+    //check if querysnapshot is null
+    if (querySnapshot != null) {
+      return ListView.builder(
+        primary: false,
+        itemCount: querySnapshot.documents.length,
+        padding: EdgeInsets.all(12),
+        itemBuilder: (context, i) {
+          return Column(
+            children: <Widget>[
+              ExpansionTileCard(
+                leading: CircleAvatar(
+                  child: Text("new"),
+                ),
+                title: Text("${querySnapshot.documents[i].data['Title']}"),
+                subtitle: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text("${querySnapshot.documents[i].data['Date']}"),
+                    Text("${querySnapshot.documents[i].data['Category']}"),
+                  ],
+                ),
+                children: <Widget>[
+                  Divider(
+                    thickness: 1.0,
+                    height: 1.0,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0,
+                        vertical: 8.0,
+                      ),
+                      child: Text(
+                          "${querySnapshot.documents[i].data['Description']}"),
+                    ),
+                  ),
+                  ButtonBar(
+                    alignment: MainAxisAlignment.spaceAround,
+                    buttonHeight: 52.0,
+                    buttonMinWidth: 90.0,
+                    children: <Widget>[
+                      FlatButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4.0)),
+                        onPressed: () {},
+                        child: Column(
+                          children: <Widget>[
+                            Icon(Icons.star),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 2.0),
+                            ),
+                            Text('Save'),
+                          ],
+                        ),
+                      ),
+                      FlatButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4.0)),
+                        onPressed: () {},
+                        child: Column(
+                          children: <Widget>[
+                            Icon(Icons.open_in_browser),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 2.0),
+                            ),
+                            Text('Open'),
+                          ],
+                        ),
+                      ),
+                      FlatButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4.0)),
+                        onPressed: () {},
+                        child: Column(
+                          children: <Widget>[
+                            Icon(Icons.share),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 2.0),
+                            ),
+                            Text('Share'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+//load data into widgets
+            ],
+          );
+        },
+      );
+    } else {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+  }
+
+  //get firestore instance
+  getDriversList() async {
+    return await Firestore.instance.collection('events').getDocuments();
   }
 }
